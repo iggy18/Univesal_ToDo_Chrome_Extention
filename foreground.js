909655338f6d761a6efbaf119796aa79b69abc83
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2019-12-31 16:08:54 -0800

    cleaned up code

--- foreground.js
+++ foreground.js
@@ -1,16 +1,19 @@
-function StrikeHandler(e) {
+function AddClassHandler(e, ElementClassString) {
     let selected = window.getSelection().anchorNode.parentNode;
-    if (selected.classList.contains("strike")) {
-        selected.classList.remove("strike");
-    } else{
-        selected.classList.add("strike");
+    if (selected.classList.contains(ElementClassString)) {
+        selected.classList.remove(ElementClassString);
+    } else {
+        selected.classList.add(ElementClassString);
     }
 }
 
 function GetKeyValue() {
     document.addEventListener('keydown',(e) => {
-        if (e.code === "KeyM" && e.ctrlKey === true) {
-            StrikeHandler(e);
+        if (e.code === "Keyu" && e.ctrlKey === true) {
+            AddClassHandler(e, "strike");
+        }
+        if (e.code === "Keyk" && e.ctrlKey === true) {
+            AddClassHandler(e, "highlight");
         }
     });
 }
